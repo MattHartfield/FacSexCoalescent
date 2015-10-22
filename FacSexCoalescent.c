4b71a3873123906b0502d14a9726612840d8146d
Matthew Hartfield <matthew.hartfield@utoronto.ca>
2015-10-22 17:39:22 -0400

    Added more code, up to rate change definition

--- FacSexCoalescent.c
+++ FacSexCoalescent.c
@@ -20,7 +20,17 @@ separately from this file.
 #include <gsl/gsl_randist.h>
 
 /* Function prototypes */
-/* TO BE ADDED */
+unsigned int trig(unsigned int x);
+double P23(unsigned int y, unsigned int k, unsigned int Na);
+double P4(unsigned int x, unsigned int k, unsigned int Na);
+double P56(unsigned int y, unsigned int Na);
+double P7(unsigned int x, unsigned int k, unsigned int Na);
+double P8(unsigned int x, unsigned int y, unsigned int k, unsigned int Na);
+double P9(unsigned int x, unsigned int k, double gee);
+double P10(unsigned int x, unsigned int y, unsigned int k, double mee);
+double P11(unsigned int y, unsigned int k, double sexC, double ree, unsigned int lrec, unsigned int nlrec, unsigned int nlrec2);
+void probset2(unsigned int N, double g, double *sexC, double rec, unsigned int lrec, unsigned int *nlrec, unsigned int *nlrec2, double mig, unsigned int *Nwith, unsigned int *Nbet, unsigned int *kin, unsigned int sw, double **pr);
+void rate_change(unsigned int pST,double pLH, double pHL, double *sexH, double *sexL, unsigned int Na, unsigned int d, unsigned int switch1, double *sexCN, double *tts, unsigned int *npST,const gsl_rng *r);
 
 /* Global variable declaration */
 unsigned int N = 0;			/* Population Size */
@@ -37,9 +47,135 @@ unsigned int Itot = 0;		/* Number of initial samples */
 unsigned int Iindv = 0;		/* Number of initial individuals */
 unsigned int Nreps = 0;		/* Number of simulation replicates */
 
+/* 'Triangle function' calculation */
+unsigned int trig(unsigned int x){
+	return (x*(x-1))/2.0;
+}
+
+/* Functions of each transition probability calculation */
+double P23(unsigned int y, unsigned int k, unsigned int Na){
+	/* One of the paired samples is recreated: (x,y) -> (x-k+1, y + 2(k-1)). 
+	OR A unique samples coaleses with another (either pre-existing or new): (x,y) -> (x-k, y + 2k - 1) */
+	return (k*y)/(1.0*Na) + trig(2.0*k)/(2.0*Na);
+}
+double P4(unsigned int x, unsigned int k, unsigned int Na){
+	/* A paired sample coaleses with new unique sample: (x,y) -> (x-k,y + 2k -1) */
+	return ((x-k)*2.0*k)/(1.0*Na);
+}
+double P56(unsigned int y, unsigned int Na){
+	/* Two pre-existing unique samples re-create a paired sample: (x,y) -> (x - k + 1, y + 2(k-1)). 
+	OR Two pre-existing paired samples coalesce: (x,y) -> (x - k, y + 2k-1) */
+	return trig(y)/(2.0*Na);
+}
+double P7(unsigned int x, unsigned int k, unsigned int Na){
+	/* Two remaining paired samples doubly coalesce asexually: (x,y) -> (x-k-1,y+2k) */
+	return trig(x-k)/(1.0*Na);
+}
+double P8(unsigned int x, unsigned int y, unsigned int k, unsigned int Na){
+	/* One of the x - k remaining paired samples can coalesce with a unique sample: (x,y) -> (x-k,y+2k-1) */
+	return ((x-k)*y)/(1.0*Na);
+}
+double P9(unsigned int x, unsigned int k, double gee){
+	/* Paired sample coaleses via gene conversion: (x,y) -> (x-k-1,y+2k+1) */
+	/* NEEDS TO BE UPDATED TO ACCOUNT FOR MULTIPLE SITES */
+	return gee*(x-k);
+}
+double P10(unsigned int x, unsigned int y, unsigned int k, double mee){
+	/* A sample migrates to another deme */
+	return mee*(x + y + k);
+}
+double P11(unsigned int y, unsigned int k, double sexC, double ree, unsigned int lrec, unsigned int nlrec, unsigned int nlrec2){
+	/* One of the single samples splits by recombination, creates two new single samples: (x,y) -> (x-k,y+2k+1) */
+	return (sexC*rec*((lrec - 1)*(y + 2*k) - nlrec) + rec*((lrec - 1)*(2*k) - nlrec2));
+}
+
+/* Calculate probability change vectors each time OVER EACH DEME */
+void probset2(unsigned int N, double g, double *sexC, double rec, unsigned int lrec, unsigned int *nlrec, unsigned int *nlrec2, double mig, unsigned int *Nwith, unsigned int *Nbet, unsigned int *kin, unsigned int sw, double **pr){
+	unsigned int x;				/* Deme counter */
+	unsigned int ksum = 0;		/* Total number of segregating events */
+	
+	/* First calculate number of splits... */
+	for(x = 0; x < d; x++){
+		ksum += *(kin + x);
+	}
+	
+	/* Calculate each transition probability, per deme */
+	for(x = 0; x < d; x++){
+		*((*(pr + 5)) + x) = P56(*(Nbet + x),N);
+		*((*(pr + 6)) + x) = P56(*(Nbet + x),N);
+		*((*(pr + 7)) + x) = P7(*(Nwith + x),*(kin + x),N);
+		*((*(pr + 8)) + x) = P8(*(Nwith + x),*(Nbet + x),*(kin + x),N);
+		*((*(pr + 9)) + x) = P9(*(Nwith + x),*(kin + x),g);
+		*((*(pr + 10)) + x) = P10(*(Nwith + x),*(Nbet + x),*(kin + x),mig);		
+		*((*(pr + 11)) + x) = P11(*(Nbet + x),*(kin + x),*(sexC + x),rec,lrec,*(nlrec + x),*(nlrec2 + x));
+		
+		/* Only activate the first three events if need to consider segregation via sex 
+		(fourth is 'split pairs remain split') */
+		if(sw == 1){
+			if(ksum != 1){
+				*((*(pr + 2)) + x) = P23(*(Nbet + x),*(kin + x),N);
+			}else if(ksum == 1){
+				*((*(pr + 2)) + x) = 0;
+			}
+			*((*(pr + 3)) + x) = P23(*(Nbet + x),*(kin + x),N);
+			*((*(pr + 4)) + x) = P4(*(Nwith + x),*(kin + x),N);
+		}else if(sw == 0){
+			*((*(pr + 2)) + x) = 0;
+			*((*(pr + 3)) + x) = 0;
+			*((*(pr + 4)) + x) = 0;
+		}
+	}
+}	/* End of 'probset2' function */
+
+/* Function to change rates of sex given a state change */
+void rate_change(unsigned int pST,double pLH, double pHL, double *sexH, double *sexL, unsigned int Na, unsigned int d, unsigned int switch1, double *sexCN, double *tts, unsigned int *npST, const gsl_rng *r){
+	unsigned int x = 0;			/* Deme counter */
+	
+	/* Setting up transition time (tts, or 'time to switch')	*/
+	if(pST == 0){
+		for(x = 0; x < d; x++){
+			*(sexCN + x) = *(sexL + x);
+		}
+		*tts = gsl_ran_geometric(r,pLH)/(2.0*Na*d);
+		*npST = 1;
+	}else if(pST == 1){
+		for(x = 0; x < d; x++){
+			*(sexCN + x) = *(sexH + x);
+		}
+		*tts = gsl_ran_geometric(r,pHL)/(2.0*Na*d);
+		*npST = 0;
+	}else if(pST == 2){		/* If stepwise change, alter depending on whether already switched or not */
+		*npST = pST;
+		if(switch1 == 0){
+			for(x = 0; x < d; x++){
+				*(sexCN + x) = *(sexL + x);
+			}
+			*tts = pLH;
+		}else if(switch1 == 1){
+			for(x = 0; x < d; x++){
+				*(sexCN + x) = *(sexH + x);
+			}
+			*tts = 0;
+		}
+	}else if(pST == 3){		/* If constant, no time to sex switch */
+		for(x = 0; x < d; x++){
+				*(sexCN + x) = *(sexL + x);
+			}
+		*tts = 0;
+		*npST = pST;
+	}
+}	/* End of 'rate_change' function */
+
 /* Main program */
 int main(int argc, char *argv[]){
-	unsigned int x;		/* Assignment counter */
+	unsigned int x, i;		/* Assignment counter, rep counter */
+	unsigned int pST, npST = 0;		/* State of reproduction heterogeneity */	
+	double Ttot = 0;			/* Time in past, initiate at zero */
+	unsigned int Ntot = 0;		/* Total number of samples at time */
+	unsigned int Nindv = 0;		/* Total number of individuals */
+	unsigned int lrec = 0;		/* Length of recombinable genome */
+	double tls = 0;				/* 'Time since Last Switch' or tls */
+	double tts = 0;				/* 'Time to switch' */
 
 	/* GSL random number definitions */
 	const gsl_rng_type * T; 
@@ -138,8 +274,10 @@ int main(int argc, char *argv[]){
 	THEN sexL[i], sexH[i] = low-sex, high-sex rate in each deme
 	*/
 	
-	double *Iwith = calloc(d,sizeof(unsigned int));		/* Within-individual samples */
-	double *Ibet = calloc(d,sizeof(unsigned int));		/* Between-individual samples */
+	unsigned int *Iwith = calloc(d,sizeof(unsigned int));		/* Within-individual samples */
+	unsigned int *Ibet = calloc(d,sizeof(unsigned int));		/* Between-individual samples */
+	unsigned int *Nwith = calloc(d,sizeof(unsigned int));		/* To be used in sim */
+	unsigned int *Nbet = calloc(d,sizeof(unsigned int));		/* To be used in sim */
 	double *sexL = calloc(d,sizeof(double));		/* Low-sex rates */	
 	double *sexH = calloc(d,sizeof(double));		/* High-sex rates */
 	
@@ -164,10 +302,12 @@ int main(int argc, char *argv[]){
 			fprintf(stderr,"All low-sex values must be less than or equal to high-sex values. Please re-check.\n");
 			exit(1);
 		}
+		/*
 		if( (*(Iwith + x) < 0) || (*(Ibet + x) < 0) ){
 			fprintf(stderr,"Number of within- and between-host samples has to be positive.\n");
 			exit(1);
 		}
+		*/
 	}
 	
 	/* Number of samples/reps to take */
@@ -184,6 +324,13 @@ int main(int argc, char *argv[]){
 	}
 	
 	/* Arrays definition and memory assignment */
+	unsigned int *nlrec = calloc(d,sizeof(unsigned int));			/* Non-recombinable samples 1 */
+	unsigned int *nlrec2 = calloc(d,sizeof(unsigned int));			/* Non-recombinable samples 2 */	
+	double *sexC = calloc(d,sizeof(double));						/* Current rates of sex per deme */	
+	double **pr = calloc(11,sizeof(double *));						/* Probability matrix per deme */
+	for (x = 0; x < 11; x++){										/* Assigning space for each population within each deme */
+		pr[x] = calloc(d,sizeof(double));
+	}
 	  
 	/* create a generator chosen by the 
     environment variable GSL_RNG_TYPE */
@@ -194,10 +341,57 @@ int main(int argc, char *argv[]){
 	r = gsl_rng_alloc(T);
 	printf("%lu\n",gsl_rng_default_seed);
 	
+	/* 
+	In R code I defined lists here for:
+	(1) NEWICK tree of each run; and
+	(2) List of mutation matrices.
+	I'll leave these for now: my plan now is to form each of these after each run, 
+	and print out to individual files instead.
+	(If I need to create such an object, probably need to create a 'struct' - need
+	to relearn how to do such things!)
+	*/
+	
+	/* Running the simulation Nreps times */
+	for(i = 0; i < Nreps; i++){
+
+		/* Setting up type of sex heterogeneity */
+		if(pSTIN == 0){
+			pST = gsl_ran_bernoulli(r,pLH/(pLH+pHL));	/* Randomly assigning initial low-sex or high-sex state */
+		}else if(pSTIN == 1){
+			pST = 2;
+		}else if(pSTIN == 2){
+			pST = 3;
+		}
+		Ttot = 0;
+		Ntot = Itot;
+		Nindv = Iindv;
+		lrec = nsites;
+	
+		for(x = 0; x < d; x++){
+			*(Nwith + x) = *(Iwith + x);	/* Resetting number of within-host samples */
+			*(Nbet + x) = *(Ibet + x);		/* Resetting number of between-host samples */
+			*(nlrec + x) = 0;				/* Genome not affected by recombination */
+		}
+		
+		/* Setting up temporal heterogeneity */
+		rate_change(pST,pLH,pHL,sexH,sexL,N,d,0,sexC,&tts,&npST,r);
+		pST = npST;
+		tls = 0;
+	}
+	
 	/* Freeing memory and wrapping up */
  	gsl_rng_free(r);
+ 	for(x = 0; x < 11; x++){
+		free(pr[x]);
+	}
+	free(pr);
+	free(sexC);
+	free(nlrec2);
+	free(nlrec);
  	free(sexH);
  	free(sexL);
+ 	free(Nbet);
+ 	free(Nwith);
  	free(Ibet);
  	free(Iwith); 	 	 	
  	
